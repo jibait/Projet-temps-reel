1065e4e04e2929ce89b9c11671926ace5bf5ef3f
Clément CUVIER <cuvierclement@proton.me>
2024-09-13 09:44:53 +0200

    Add ping user and ping everyone

--- ihm/app/components/ChatMessage.tsx
+++ ihm/app/components/ChatMessage.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import { Flex, Avatar, Box, Text, Image } from '@chakra-ui/react';
 import { Message } from '../store/types';
+import { useStore } from '../Hooks/useStore';
 
-const ChatMessage: React.FC<Message> = ({ pseudo, message, profilePicture: photoProfil, image }) => (
-    <Flex my="4" p="3" bg="gray.100" borderRadius="md" align="center">
-        <Avatar src={photoProfil} name={pseudo} size="md" mr="4" />
-        <Box>
-            <Text fontWeight="bold">{pseudo}</Text>
-            {image ? <Image src={image} alt="message" boxSize="150px" /> : <Text>{message}</Text>}
-        </Box>
-    </Flex>
-);
+
+
+const ChatMessage: React.FC<Message> = ({ pseudo, message, profilePicture: photoProfil, image }) => {
+    let highlight = false;
+    const user = useStore().pseudo;
+    if (message?.includes(`@` + user) || message?.includes(`@everyone`)) {
+        console.log(`User ${pseudo} was mentioned in the message.`);
+        highlight = true
+    }
+
+    return (
+        <Flex my="4" p="3" bg={highlight ? "red.100" : "gray.100"} borderRadius="md" align="center">
+            <Avatar src={photoProfil} name={pseudo} size="md" mr="4" />
+            <Box>
+                <Text fontWeight="bold">{pseudo}</Text>
+                {image ? <Image src={image} alt="message" boxSize="150px" /> : <Text>{message}</Text>}
+            </Box>
+        </Flex>
+    );
+};
 
 export default ChatMessage;
